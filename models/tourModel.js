11897ecbaf007af7369a78e33c31f0d7066fcdeb
Enu Azubike <enuazubike88@gmail.com>
2021-08-09 15:03:07 +0100

    extended the tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,14 +5,57 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     type: String,
     unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, ' A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, ' A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'should have a difficulty'],
   },
   price: {
     required: [true, 'A tour must have a price'],
     type: Number,
   },
-  rating: {
+  ratingsAvg: {
     default: 4.5,
     type: Number,
+  },
+  ratingsQuantity: {
+    default: 0,
+    type: Number,
+  },
+  discount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'a tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'a tour must have a cover image'],
+  },
+  images: {
+    type: [String],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: {
+    type: [Date],
   },
 });
 
