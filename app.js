a37b5141efe42c751b61143e1f793e5a56cff0c8
Enu Azubike <enuazubike88@gmail.com>
2021-08-13 20:20:18 +0100

    created global error handling middleware

--- app.js
+++ app.js
@@ -21,10 +21,22 @@ app.use('/api/v1/users', userRouter);
 
 // handle page not found for all other routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'Fail',
-    message: `Cant find ${req.originalUrl} on this server`,
+  // res.status(404).json({
+  //   status: 'Fail',
+  //   message: `Cant find ${req.originalUrl} on this server`,
+  // });
+  const err = new Error(`Cant find ${req.originalUrl} on this server`);
+  err.statusCode = 404;
+  err.status = 'Fail';
+  next(err);
+});
+// global error handler
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
-
 module.exports = app;
