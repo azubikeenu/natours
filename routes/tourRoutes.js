5ff52d39a4050ace2ab2b0d3fd8dac05bf3eb52d
Enu Azubike <enuazubike88@gmail.com>
2021-08-31 12:23:11 +0100

    Implemented nested routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -12,16 +12,26 @@ const {
 
 const { protect, restrictTo } = require('../controllers/authController');
 
+const { createReview } = require('../controllers/reviewController');
+
 const router = express.Router();
 // router.param('id', checkID);
 router.route('/tour-stat').get(getTourStats);
 router.route('/best-cheap').get(bestCheap, getAllTours);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/').get(protect, getAllTours).post(createTour);
+
 router
   .route('/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
+
+// nested reviews routes
+//router.route('/:tourId/reviews').get(getAllReviews);
+
+router
+  .route('/:tourId/reviews')
+  .post(protect, restrictTo('user'), createReview);
 
 module.exports = router;
