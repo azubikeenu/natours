ca6917d5f18044f89a35edcff30430b57bd1ea1b
Enu Azubike <enuazubike88@gmail.com>
2021-09-08 02:45:54 +0100

    added the overview page and the tour page

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const hpp = require('hpp');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRouter');
+
+const viewRouter = require('./routes/viewRoutes');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -70,13 +73,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .render('base', { tour: 'The Forest Hiker', user: 'Enu Richard' });
-});
-
 //ROUTES
+
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 
 app.use('/api/v1/users', userRouter);
---
+++ controllers/viewsController.js
@@ -0,0 +1,22 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsyc');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // get tour from database
+  const tours = await Tour.find();
+  // build the template
+  //render the template using extracted data
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  const { slug } = req.params;
+  const tour = await Tour.findOne({ slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  res.status(200).render('tour', { title: 'The Forest Hiker', tour });
+});
